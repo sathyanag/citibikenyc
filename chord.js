33eeb2d93877f3d06fd443246ec386e909b60556
Sathya Kadaba <39969743+sathyanag@users.noreply.github.com>
2018-06-06 00:23:43 -0400

    Update chord.js

--- chord.js
+++ chord.js
@@ -4,117 +4,122 @@
    http://bl.ocks.org/carsonfarmer/11478345
 */
 
-var data,
-    matrices,
-    stations,
-    years;
+var arcs = [],
+    chords = [];
+
+var data, matrices, neighs, years;
 
 var last_chord = {};
 
-var formatPercent = d3.format(".1%");
+var fill = d3.scale.category20()
+    .domain(d3.range(42));
 
-var width = 1100,
-    height = 1000,
-    padding = 150,
-    r0 = (Math.min(width, height)-padding * 2) * 0.41,
-    r1 = r0 * 1.05;
+var w = 960,
+    h = 500,
+    padding = 80,
+    r0 = (Math.min(w, h)-padding*2) * .41,
+    r1 = r0 * 1.1;
 
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("id", "circle")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var svg = d3.select("#chart")
+    .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append("svg:g")
+    .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+    .append("g")
+      .attr("id", "circle")
+    
+  svg.append("circle")
+      .attr("r", r1);
 
-svg.append("circle")
-    .attr("r", r1);
-
-// add slider functionality
 d3.select("#slider").on('change', function(d) {
-    var current = years[parseInt(this.value)],
-        year = current.substring(0, 4),
-        month = current.substring(5);
+          var current = years[parseInt(this.value)],
+          year = current.substring(0, 4),
+          month = current.substring(5);
+          d3.select("#month").text(""+month);
+          d3.select("#year").text(""+year);
+  var new_data = matrices[current]
 
-    d3.select("#month").text("" + month);
-    d3.select("#year").text("" + year);
-
-    var new_data = matrices[current];
-
-    rerender(new_data);
+  rerender(new_data);
 });
 
-d3.csv("stations.csv", function(rides) {
-  d3.json("rides_ym.json", function(matrix) {
-    matrices = matrix,
+d3.csv("stations.csv", function(nat) {
+  d3.json("rides_ym.json", function(mat) {
+    matrices = mat,
     years    = Object.keys(matrices).sort(),
     current  = years[0],
-    stations = rides;
+    neighs   = nat;
 
-    d3.select("#slider")
-      .attr("max", years.length-1);
+    d3.select("#slider").attr("max", years.length-1);
 
     // Compute the chord layout.
-    data = matrices[current];
+    data = matrices[current]
 
     var chord = d3.layout.chord()
-      .padding(0.02)
+      .padding(.05)
       .sortSubgroups(d3.descending)
       // .sortChords(d3.ascending)
       .matrix(data);
-
+   
     // draw arcs
-    svg.append("g")
+    svg.append("svg:g")
        .selectAll("path")
        .data(chord.groups)
-     .enter().append("path")
-       .on("mouseover", mouseover)
+       .enter().append("svg:path")
+       .on("mouseover", function(d, i) { mouseover(d, i, chord); })
        .attr("class", "arc")
-       .style("fill", function(d, i) { return rides[i].color; })
-       .style("stroke", function(d, i) { return rides[i].color; })
+       .style("fill", function(d) { return fill(d.index); })
+       .style("stroke", function(d) { return fill(d.index); })
        .attr("d", d3.svg.arc().innerRadius(r0).outerRadius(r1))
        .append("title").text(function(d, i) {
-         return stations[i].name + ": " + formatPercent(d.value) + " of origins";
+         return neighs[i].name + ": " + formatPercent(d.value) + " of origins";
        });
 
     // draw chords
-    svg.append("g")
+    svg.append("svg:g")
        .attr("class", "chord")
        .selectAll("path")
        .data(chord.chords)
-     .enter().append("path")
+       .enter().append("svg:path")
        .attr("d", d3.svg.chord().radius(r0))
-       .style("fill", function(d, i) { return rides[d.source.index].color; })
+       .style("fill", function(d) { return fill(d.source.index); })
        .style("stroke", '#333')
-       .attr("visibility", function(d, i) { return d.source.value > 0.000001 ? "visible" : "hidden"; })
+       .attr("visibility", function(d, i) { return d.source.value > 0.0029 ? "visible" : "hidden"; })
        .style("opacity", 1)
        .append("title").text(function(d) {
-        return stations[d.source.index].name
-          + " → " + stations[d.target.index].name
+        return neighs[d.source.index].name
+          + " → " + neighs[d.target.index].name
           + ": " + formatPercent(d.source.value)
-          + "\n" + stations[d.target.index].name
-          + " → " + stations[d.source.index].name
+          + "\n" + neighs[d.target.index].name
+          + " → " + neighs[d.source.index].name
           + ": " + formatPercent(d.target.value);
         });
 
 
-    var ticks = svg.append("g")
+    var ticks = svg.append("svg:g")
         .attr("class", "ticks")
         .selectAll("g")
         .data(chord.groups)
-        .enter().append("g")
-          .on("mouseover", mouseover)
+        .enter().append("svg:g")
+          .on("mouseover", function(d, i) { mouseover(d, i, chord); })
           .attr("class", "group")
         .selectAll("g")
         .data(groupTicks)
-        .enter().append("g")
-        .attr("class", "stations")
+        .enter().append("svg:g")
+        .attr("class", "neighs")
         .attr("transform", function(d) {
           return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
               + "translate(" + r1 + ",0)";
         });
 
+    ticks.append("svg:line")
+         .attr("x1", 1)
+         .attr("y1", 0)
+         .attr("x2", 5)
+         .attr("y2", 0)
+         .attr("stroke", '#000')
 
-    ticks.append("text")
+    ticks.append("svg:text")
          .attr("x", 8)
          .attr("dy", '.35em')
          .attr("text-anchor", function(d) {
@@ -133,7 +138,7 @@ d3.csv("stations.csv", function(rides) {
 function rerender(data) {
 
   var chord = d3.layout.chord()
-    .padding(.02)
+    .padding(.05)
     .sortSubgroups(d3.descending)
     // .sortChords(d3.ascending)
     .matrix(data);
@@ -142,7 +147,7 @@ function rerender(data) {
   svg.selectAll(".ticks")
      .selectAll(".group")
      .data(chord.groups)
-    .selectAll(".stations")
+    .selectAll(".neighs")
      .data(groupTicks)
      .transition()
      .duration(1500)
@@ -155,10 +160,10 @@ function rerender(data) {
   svg.selectAll(".arc")
      .data(chord.groups)
      .transition()
-     .duration(1000)
+     .duration(1500)
      .attrTween("d", arcTween(last_chord))
      .select("title").text(function(d, i) {
-         return stations[i].name + ": " + formatPercent(d.value) + " of origins";
+         return neighs[i].name + ": " + formatPercent(d.value) + " of origins";
        });
 
   // update chords
@@ -166,14 +171,14 @@ function rerender(data) {
      .selectAll("path")
      .data(chord.chords)
      .transition()
-     .duration(1000)
+     .duration(1500)
      .attrTween("d", chordTween(last_chord))
      .select("title").text(function(d) {
-        return stations[d.source.index].name
-          + " → " + stations[d.target.index].name
+        return neighs[d.source.index].name
+          + " → " + neighs[d.target.index].name
           + ": " + formatPercent(d.source.value)
-          + "\n" + stations[d.target.index].name
-          + " → " + stations[d.source.index].name
+          + "\n" + neighs[d.target.index].name
+          + " → " + neighs[d.source.index].name
           + ": " + formatPercent(d.target.value);
         });
 
@@ -181,8 +186,8 @@ function rerender(data) {
 }
 
 var arc =  d3.svg.arc()
-      .startAngle(function(d) { return d.startAngle; })
-      .endAngle(function(d) { return d.endAngle; })
+      .startAngle(function(d) { return d.startAngle })
+      .endAngle(function(d) { return d.endAngle })
       .innerRadius(r0)
       .outerRadius(r1);
 
@@ -192,7 +197,9 @@ function arcTween(chord) {
   return function(d,i) {
     var i = d3.interpolate(chord.groups()[i], d);
 
-    return function(t) { return arc(i(t)); };
+    return function(t) {
+      return arc(i(t));
+    }
   }
 }
 
@@ -200,7 +207,9 @@ function chordTween(chord) {
   return function(d,i) {
     var i = d3.interpolate(chord.chords()[i], d);
 
-    return function(t) { return chordl(i(t)); };
+    return function(t) {
+      return chordl(i(t));
+    }
   }
 }
 
@@ -208,14 +217,21 @@ function groupTicks(d) {
   var k = (d.endAngle - d.startAngle) / d.value;
     return [{
       angle: d.value * k / 2 + d.startAngle,
-      label: stations[d.index].name
+      label: neighs[d.index].name
     }];
 }
 
-function mouseover(d, i) {
-  d3.selectAll(".chord path")
-    .classed("fade", function(p) {
-    return p.source.index != i
-        && p.target.index != i;
-  });
+function rand() {
+  var val = Math.floor(Math.random() * 7)
+  return val
 }
+
+var formatPercent = d3.format(".1%");
+
+function mouseover(d, i) {
+      d3.selectAll(".chord path")
+        .classed("fade", function(p) {
+        return p.source.index != i
+            && p.target.index != i;
+      });
+    }
